1b14e9bb6af155eea1295a45aa9089167e9eb6c9
tranvandinh0298 <tranvandinh0298@gmail.com>
2023-09-19 23:12:26 +0700

    update progress

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -21,7 +22,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('secret'), // password
             'remember_token' => Str::random(10),
             'verified' => $verified = fake()->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
             'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
